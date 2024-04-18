7b37b5e331d524dede5b4851c3bb7ae383a3ed4c
bbbang105 <2018111366@dgu.ac.kr>
2024-04-19 01:18:46 +0900

    #1 [feat] : 네이버 유저 정보 DTO 추가

---
+++ src/main/java/kusitms/jangkku/global/auth/dto/NaverUserInfo.java
@@ -0,0 +1,26 @@
+package kusitms.jangkku.global.auth.dto;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Map;
+
+@AllArgsConstructor
+public class NaverUserInfo implements OAuth2UserInfo {
+
+    private Map<String, Object> attributes;
+
+    @Override
+    public String getProviderId() {
+        return (String) attributes.get("id");
+    }
+
+    @Override
+    public String getProvider() {
+        return "naver";
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+}
