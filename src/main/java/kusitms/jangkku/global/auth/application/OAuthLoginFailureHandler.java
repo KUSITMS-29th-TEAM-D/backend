6fce3ec7fcbdd92894c07d27844801f46f32ee54
bbbang105 <2018111366@dgu.ac.kr>
2024-04-19 01:19:35 +0900

    #1 [feat] : 소셜 로그인 실패 핸들러 구현

---
+++ src/main/java/kusitms/jangkku/global/auth/application/OAuthLoginFailureHandler.java
@@ -0,0 +1,24 @@
+package kusitms.jangkku.global.auth.application;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuthLoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.error("LOGIN FAILED : {}", exception.getMessage());
+        super.onAuthenticationFailure(request, response, exception);
+    }
+}
