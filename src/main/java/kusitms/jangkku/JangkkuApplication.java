b6aa70920360c679e1d4591409493671bc51c0eb
bbbang105 <2018111366@dgu.ac.kr>
2024-04-18 18:16:01 +0900

    [init] : 프로젝트 생성

---
+++ src/main/java/kusitms/jangkku/JangkkuApplication.java
@@ -0,0 +1,13 @@
+package kusitms.jangkku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangkkuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JangkkuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kusitms/jangkku/JangkkuApplicationTests.java
@@ -0,0 +1,13 @@
+package kusitms.jangkku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JangkkuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
