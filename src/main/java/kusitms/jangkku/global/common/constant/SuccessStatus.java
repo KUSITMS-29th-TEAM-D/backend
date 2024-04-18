0c7d0022754e825414780e1e357d07bf3c0244b6
bbbang105 <2018111366@dgu.ac.kr>
2024-04-19 01:21:58 +0900

    #1 [feat] : 성공 시 응답 Enum 추가

---
+++ src/main/java/kusitms/jangkku/global/common/constant/SuccessStatus.java
@@ -0,0 +1,41 @@
+package kusitms.jangkku.global.common.constant;
+
+import kusitms.jangkku.global.common.code.BaseCode;
+import kusitms.jangkku.global.common.dto.ReasonDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+    // 전역 응답 코드
+    _OK(HttpStatus.OK, "200", "성공입니다."),
+    _CREATED(HttpStatus.CREATED, "201", "생성에 성공했습니다."),
+
+    // 커스텀 응답 코드
+    _CREATED_ACCESS_TOKEN(HttpStatus.CREATED, "201", "액세스 토큰 재발행에 성공했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDto getReason() {
+        return ReasonDto.builder()
+                .isSuccess(true)
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+    @Override
+    public ReasonDto getReasonHttpStatus() {
+        return ReasonDto.builder()
+                .isSuccess(true)
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
