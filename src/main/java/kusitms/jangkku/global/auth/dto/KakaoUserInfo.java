38e854a63cf573783edacdb0b4d8788a2019b2dd
bbbang105 <2018111366@dgu.ac.kr>
2024-04-19 01:18:40 +0900

    #1 [feat] : 카카오 유저 정보 DTO 추가

---
+++ src/main/java/kusitms/jangkku/global/auth/dto/KakaoUserInfo.java
@@ -0,0 +1,28 @@
+package kusitms.jangkku.global.auth.dto;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Map;
+
+@AllArgsConstructor
+public class KakaoUserInfo implements OAuth2UserInfo {
+
+    private Map<String, Object> attributes;
+
+    @Override
+    public String getProviderId() {
+        // Long 타입이기 때문에 toString으로 변환
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+    @Override
+    public String getName() {
+        // kakao_account라는 Map에서 추출
+        return (String) ((Map) attributes.get("properties")).get("nickname");
+    }
+}
