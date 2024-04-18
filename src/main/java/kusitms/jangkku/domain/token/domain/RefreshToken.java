e80f39148dca17dc7561ac29218548f18d3a29d8
bbbang105 <2018111366@dgu.ac.kr>
2024-04-19 01:17:45 +0900

    #1 [feat] : 리프레쉬 토큰 엔터티 추가

---
+++ src/main/java/kusitms/jangkku/domain/token/domain/RefreshToken.java
@@ -0,0 +1,25 @@
+package kusitms.jangkku.domain.token.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+@Table(name = "refresh_tokens")
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "refresh_tokens_id")
+    private Long id;
+
+    @Column(name = "users_uuid", columnDefinition = "BINARY(16)", unique = true)
+    private UUID userId;
+
+    @Column(name = "token", nullable = false)
+    private String token;
+}
