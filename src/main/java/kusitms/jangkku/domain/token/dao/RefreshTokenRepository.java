ed5dfb265dd1db67f3e09d927c8c124097dbbe1f
bbbang105 <2018111366@dgu.ac.kr>
2024-04-30 06:55:09 +0900

    #8 [feat] : Redis에 접근하기 위한 레포지토리 구현

--- src/main/java/kusitms/jangkku/domain/token/dao/RefreshTokenRepository.java
+++ src/main/java/kusitms/jangkku/domain/token/dao/RefreshTokenRepository.java
@@ -1,21 +1,40 @@
 package kusitms.jangkku.domain.token.dao;
 
-import jakarta.transaction.Transactional;
 import kusitms.jangkku.domain.token.domain.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
 
+import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 
 @Repository
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
-    @Query("SELECT u FROM RefreshToken u WHERE u.userId = :userId")
-    RefreshToken findByUserId(UUID userId);
+@RequiredArgsConstructor
+public class RefreshTokenRepository {
+    @Value("${jwt.refresh-token.expiration-time}")
+    private long REFRESH_TOKEN_EXPIRATION_TIME; // 리프레쉬 토큰 유효기간
 
-    @Transactional
-    @Modifying
-    @Query("DELETE FROM RefreshToken u WHERE u.userId = :userId")
-    void deleteByUserId(UUID userId);
+    private final RedisTemplate redisTemplate;
+
+    public void save(final RefreshToken refreshToken) {
+        ValueOperations<UUID, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(refreshToken.getUserId(), refreshToken.getRefreshToken());
+        redisTemplate.expire(refreshToken.getRefreshToken(), REFRESH_TOKEN_EXPIRATION_TIME, TimeUnit.MILLISECONDS);
+    }
+
+    public Optional<RefreshToken> findByUserId(final UUID userId) {
+        ValueOperations<UUID, String> valueOperations = redisTemplate.opsForValue();
+        String refreshToken = valueOperations.get(userId);
+
+        if (Objects.isNull(refreshToken)) {
+            return Optional.empty();
+        }
+
+        return Optional.of(new RefreshToken(userId, refreshToken));
+    }
 }
