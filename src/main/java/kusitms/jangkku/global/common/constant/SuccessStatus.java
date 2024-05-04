8785fb287111c9a098edea6cb94314a89e6ce9f2
bbbang105 <2018111366@dgu.ac.kr>
2024-05-05 06:04:55 +0900

    #12 [feat] : 유저 등록 성공 Enum 추가

--- src/main/java/kusitms/jangkku/global/common/constant/SuccessStatus.java
+++ src/main/java/kusitms/jangkku/global/common/constant/SuccessStatus.java
@@ -13,7 +13,8 @@ public enum SuccessStatus implements BaseCode {
     _OK(HttpStatus.OK, "200", "성공입니다."),
     _CREATED(HttpStatus.CREATED, "201", "생성에 성공했습니다."),
 
-    // 커스텀 응답 코드
+    // 유저 & 토큰
+    SUCCESS_REGISTER_USER(HttpStatus.CREATED, "201", "유저 등록에 성공했습니다."),
     CREATED_ACCESS_TOKEN(HttpStatus.CREATED, "201", "액세스 토큰 재발행에 성공했습니다.");
 
     private final HttpStatus httpStatus;
