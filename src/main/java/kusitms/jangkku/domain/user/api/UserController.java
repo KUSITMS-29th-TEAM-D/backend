7e3a9412b234346108f2ed03042cecd6c49e9a86
bbbang105 <2018111366@dgu.ac.kr>
2024-05-06 13:50:11 +0900

    [style] : 엔드포인트 수정

--- src/main/java/kusitms/jangkku/domain/user/api/UserController.java
+++ src/main/java/kusitms/jangkku/domain/user/api/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
