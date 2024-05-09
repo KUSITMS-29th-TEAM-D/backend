83d435938b6ce52cc71ce70625fe6c1270e7d65b
bbbang105 <2018111366@dgu.ac.kr>
2024-05-10 04:53:04 +0900

    #15 [feat] : 현재 페르소나 엔터티 & 레포지토리 추가

---
+++ src/main/java/kusitms/jangkku/domain/persona/dao/DefinePersonaRepository.java
@@ -0,0 +1,11 @@
+package kusitms.jangkku.domain.persona.dao;
+
+import kusitms.jangkku.domain.persona.domain.DefinePersona;
+import kusitms.jangkku.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DefinePersonaRepository extends JpaRepository<DefinePersona,Long> {
+    DefinePersona findByUser(User user);
+}
