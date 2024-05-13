4506b373ac67b81ecf2d804ff61e9aa14ddf9ca5
emilywin825 <emilywin825@naver.com>
2024-05-13 21:39:07 +0900

    [feat]ChatBotController 생성

---
+++ src/main/java/kusitms/jangkku/domain/chatbot/api/ChatBotController.java
@@ -0,0 +1,24 @@
+package kusitms.jangkku.domain.chatbot.api;
+
+import kusitms.jangkku.domain.chatbot.application.ChatBotService;
+import kusitms.jangkku.domain.chatbot.dto.ChatBotDto;
+import kusitms.jangkku.global.common.ApiResponse;
+import kusitms.jangkku.global.common.constant.SuccessStatus;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/chat-bot")
+@Slf4j
+@RequiredArgsConstructor
+public class ChatBotController {
+
+    private final ChatBotService chatBotService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<String>> testChatBonController(@RequestBody ChatBotDto.ChatBotRequestDto chatBotRequestDto){
+        return ApiResponse.onSuccess(SuccessStatus._OK,chatBotService.requestChatBot(chatBotRequestDto)) ;
+    }
+}
