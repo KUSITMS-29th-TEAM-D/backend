05260ce1c348a0d54c78cfb69261422e02ea8f52
bbbang105 <2018111366@dgu.ac.kr>
2024-05-17 03:05:35 +0900

    #33 [feat] : 설계하기 페르소나 능력 엔터티 & 레포지토리 추가

---
+++ src/main/java/kusitms/jangkku/domain/persona/dao/DesignPersonaAbilityRepository.java
@@ -0,0 +1,7 @@
+package kusitms.jangkku.domain.persona.dao;
+
+import kusitms.jangkku.domain.persona.domain.DesignPersonaAbility;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DesignPersonaAbilityRepository extends JpaRepository<DesignPersonaAbility,Long> {
+}
