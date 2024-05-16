5b8024f91b0f2613527b69a7930cb73c38aaf96c
bbbang105 <2018111366@dgu.ac.kr>
2024-05-17 04:53:25 +0900

    #33 [feat] : CLOVA 시스템 메세지 추가

--- src/main/java/kusitms/jangkku/domain/chatbot/dto/Message.java
+++ src/main/java/kusitms/jangkku/domain/chatbot/dto/Message.java
@@ -2,7 +2,6 @@ package kusitms.jangkku.domain.chatbot.dto;
 
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.context.annotation.Bean;
 
 @Data
 @Builder
@@ -38,7 +37,7 @@ public class Message {
 
     public static Message creatUserOf(String content) {
         return Message.builder()
-                .role(ROLE.system)
+                .role(ROLE.user)
                 .content(content)
                 .build();
     }
@@ -50,4 +49,30 @@ public class Message {
                 .build();
     }
 
+    public static Message createDesignPersonaSystemMessage() {
+        return Message.builder()
+                .role(Message.ROLE.system)
+                .content("<주제>\n" +
+                        "사용자한테 5가지 카테고리의 응답을 받은 후에, 한 문장으로 어떤 브랜드가 되고 싶은지를 요약한다.\n" +
+                        "###\n" +
+                        "<규칙>\n" +
+                        "1. 가능하면 쉼표를 쓰지 않고, 1문장으로 끝나도록 한다.\n" +
+                        "2. 글자수는 절대로 30자를 넘지 않는다.\n" +
+                        "3. 꼭 모든 사용자의 응답이 결과물에 들어갈 필요는 없다.\n" +
+                        "4. 최종적인 커리어는 1개만 나올 수 있다.\n" +
+                        "5. 커리어와 직업 간에는 유사도가 높아야 한다.\n" +
+                        "6. 말투는 딱딱하지 않게 20~30대 같은 말투를 사용한다.\n" +
+                        "7. 최종적인 결과는 큰 따옴표로 묶인 1문장만 나온다.\n" +
+                        "### \n" +
+                        "아래는 예시 질문과 응답이야.\n" +
+                        "카테고리1. 활동하고 싶은 분야 : 여행, 요리, 사진 & 영상\n" +
+                        "카테고리 2. 사람들에게 알리고 싶은 가장 큰 특징 : 성장지향적인, 도전적인, 목표지향적인\n" +
+                        "카테고리 3. 사람들에게 알리고 싶은 나의 능력 : 목표달성, 실행력, 도전정신\n" +
+                        "카테고리 4. 나를 알리고 싶은 플랫폼 : 유튜브, 인스타그램\n" +
+                        "카테고리 5. 나를 하나의 커리어로 정의한다면 : 유튜버\n" +
+                        "###\n" +
+                        "아래는 모범 예시 결과야. 이런식으로 요약해주면 돼.\n" +
+                        "\"다양한 취미활동을 즐기며 새로운 것에 도전해 나만의 개성있는 콘텐츠를 제작하는 '취미부자 유튜버'\"")
+                .build();
+    }
 }
