fa6b470cb28b22c2885806f188e1b42087204fd4
bbbang105 <2018111366@dgu.ac.kr>
2024-05-17 16:10:13 +0900

    #35 [feat] : 조각 카드 이미지 url Enum 추가

--- src/main/java/kusitms/jangkku/domain/persona/constant/Content.java
+++ src/main/java/kusitms/jangkku/domain/persona/constant/Content.java
@@ -12,7 +12,10 @@ public enum Content {
             Arrays.asList("생산성", "지혜", "창의성"),
             "기계적 운동적인 능력은 있으나 대인관계능력은 조금 부족해요.\n연구 능력과 수학적, 과학적인 능력은 높지만 지도력이나 설득력이 부족할 때도 있어요.\n상징적 자유적 비체계적 능력은 있으나 체계적 순서적인 능력은 부족해요.",
             "예술적 창조와 표현, 변화와 다양성을 좋아하고, 틀에 박힌것을 싫어해요.\n모호하고 자유로운 활동들을 좋아하고 관찰적, 상징적 현상의 창조적인 탐구를 수반하는 활동들에 흥미를 보여요.\n그러나 사회적이고 반복적인 활동들에서는 관심이 부족한 경향이 있어요.",
-            Arrays.asList("현실형", "탐구형", "예술형")),
+            Arrays.asList("현실형", "탐구형", "예술형"),
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Creator_front.png",
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Creator_back.png"
+            ),
     INSIGHTER("RIC",
             "Insighter",
             "날카로운 현실을 바라보는 당신은, 인사이터(Insighter)",
@@ -21,7 +24,9 @@ public enum Content {
             Arrays.asList("생산성", "지혜", "안정"),
             "기계적 운동적인 능력은 있으나 대인관계능력은 조금 부족해요.\n연구 능력과 수학적, 과학적인 능력은 높지만 지도력이나 설득력이 부족할 때도 있어요.\n예술적, 독창적인 상상력보다는 사무적이고, 체계적이에요.",
             "관찰적, 체계적이며, 물리적, 문화적 현상의 탐구를 수반하는 활동들에 흥미를 보이고, 정해진 원칙과 계획에 따라 자료들을 기록, 적리, 조직하는 일을 좋아해요.\n동시에 창의적, 자율적이며 모험적, 비체계적인 활동에는 혼란을 느끼는 경향이 있어요.",
-            Arrays.asList("현실형", "탐구형", "관습형")),
+            Arrays.asList("현실형", "탐구형", "관습형"),
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Insighter_front.png",
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Insighter_back.png"),
     INNOVATOR("REA",
             "Innovator",
             "현실을 예술적인 눈으로 보는 당신은, 이노베이터(Innovator)",
@@ -30,7 +35,10 @@ public enum Content {
             Arrays.asList("전문성", "모험", "개성"),
             "외부에 흔들리지 않고, 자기만의 기준을 유지하며 운동적인 능력이 있어요.\n과학적인 능력은 다소 부족하지만, 적극적이고 사회적이에요.\n상징적, 자유로운 상상력이 강점이지만 체계적이거나 순서적이지는 않아요.",
             "신체적 기술들을 좋아하고, 조직의 목적을 위해 타인을 선도, 계획, 통제, 관리하는 일을 좋아하지만 체계적 활동에는 흥미가 없는 편이에요.\n예술적 창조와 표현, 변화와 다양성을 좋아하고, 틀에 박힌것을 싫어하며, 자유롭고, 상징적인 활동들을 좋아해요.",
-            Arrays.asList("현실형", "기업형", "예술형")),
+            Arrays.asList("현실형", "기업형", "예술형"),
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Innovator_front.png",
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Innovator_back.png"
+            ),
     INVENTOR("REC",
             "Inventor",
             "현실적이면서도 창의적인 당신은, 인벤터(Inventor)",
@@ -39,7 +47,9 @@ public enum Content {
             Arrays.asList("생산성", "모험", "능률"),
             "외부에 흔들리지 않고, 자기만의 기준을 유지하며 운동적인 능력이 있어요.\n적극적이고 사회적이고, 지도력과 언어구사능력이 뛰어나요.\n때론 사무적이며, 계산적이고 회계 정리를 잘해요.",
             "분명하고 체계적인 대상을 다루는 활동 내지는 신체적 기술들을 좋아하고, 조직의 목적과 경제적 이익을 얻기 위해 타인을 선도, 계획, 통제, 관리하는 일을 좋아해요.\n현실적인 직업환경에서 창의적, 모험적 능력을 발휘하여 문제를 해결하는 활동을 선호해요.",
-            Arrays.asList("현실형", "기업형", "관습형")),
+            Arrays.asList("현실형", "기업형", "관습형"),
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Inventor_front.png",
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Inventor_back.png"),
     PROJECTOR("SIA",
             "Projector",
             "치열한 탐구정신으로 사회를 보는 당신은, 프로젝터(Projector)",
@@ -48,7 +58,9 @@ public enum Content {
             Arrays.asList("사랑", "지혜", "창의성"),
             "사회적 교육적 지도력과 대인관계 능력이 있어요.\n상징적이고 자유롭지만, 숫자나 체계에는 다소 약해요.\n은근히 학구적,지적인 자부심을 가지고 있으며 높은 연구 능력을 발휘할 때도 있어요.",
             "타인의 문제를 듣고 이해하고 도와주고 치료해주고, 봉사하는 활동들에 관심이 많으며, 문화적 현상의 창조적인 탐구를 수반하는 활동들에 흥미를 보이지만, 기계적이고 체계적인 활동에는 흥미가 없는 편이에요.",
-            Arrays.asList("사회형", "탐구형", "예술형")),
+            Arrays.asList("사회형", "탐구형", "예술형"),
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Projector_front.png",
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Projector_back.png"),
     CONNECTOR("SIC",
             "Connector",
             "사람들과의 연결, 소통을 좋아하는 당신은, 커넥터(Connector)",
@@ -57,7 +69,9 @@ public enum Content {
             Arrays.asList("평등", "지혜", "체계"),
             "사회적 교육적 지도력과 대인관계 능력이 뛰어나면서 학구적, 지적인 자부심도 겸비하고 있어요.\n체계성, 정확성을 보유하고 있지만 때로는 예술적, 상상적인 능력이 부족할 때도 있어요.",
             "타인의 문제를 듣고 이해하고 도와주고 치료해주고, 봉사하는 활동들에 흥미를 보여요.\n체계적인 직업환경에서 정해진 원칙과 계획에 따라 조직하는 일을 좋아하고 친화력을 발휘하여 사람들간 상호 성장을 도모하는 활동을 좋아해요.",
-            Arrays.asList("사회형", "탐구형", "관습형")),
+            Arrays.asList("사회형", "탐구형", "관습형"),
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Connector_front.png",
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Connector_back.png"),
     ENCOURAGER("SEA",
             "Encourager",
             "팀을 이끌어 감동적인 변화를 만드는 당신은, 인커리져(Encourager)",
@@ -66,7 +80,9 @@ public enum Content {
             Arrays.asList("평등", "명예", "개성"),
             "사회적 교육적 지도력과 대인관계 능력이 뛰어나고, 설득력이 강해요.\n때로는 과학적, 체계적인 능력은 다소 부족할 때도 있어요.\n상징적이며 자유로우며, 비체계적인 능력에서 두각을 드러내고 있어요.",
             "타인의 문제를 듣고 이해하고 도와주고 치료해주고, 봉사하는 활동들에 흥미를 보여요.\n조직의 목적을 위해 타인을 선도, 관리하는 일과 그 결과로 얻는 위신, 인정을 얻는 활동들을 좋아해요.\n예술적 창조와 표현, 다양성을 좋아하지만, 체계적이고 구조화된 활동에는 대체적으로 흥미가 없는 편이에요.",
-            Arrays.asList("사회형", "기업형", "예술형")),
+            Arrays.asList("사회형", "기업형", "예술형"),
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Encourager_front.png",
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Encourager_back.png"),
 
     ORGANIZER("SEC",
             "Organizer",
@@ -76,7 +92,9 @@ public enum Content {
             Arrays.asList("평등", "모험", "체계"),
             "적극적이고 사회적이며, 지도력과 대인관계 능력이 뛰어나고, 설득력이 강한 편이에요.\n사무적이며, 체계적인 편이며, 정확하게 내 의사를 전해요.\n다만 현실적인 성향이라 예술적, 상상적인 능력은 부족한 편이에요.",
             "정해진 원칙과 계획에 따라 자료들을 기록, 적리, 조직하는 일을 좋아하고 체계적인 직업환경에서 사무적, 계산적 능력을 발휘하는 활동을 좋아해요.\n조직의 목적과 경제적 이익을 위해 타인의 문제를 듣고 이해하고 도와주는 일에 흥미를 보이지만 창의적, 자율적이며 모험적인 활동에는 혼란을 느끼기도 해요.",
-            Arrays.asList("사회형", "기업형", "관습형"));
+            Arrays.asList("사회형", "기업형", "관습형"),
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Organizer_front.png",
+            "https://kr.object.ncloudstorage.com/coolpiece-bucket/Organizer_back.png");
 
     private final String code;
     private final String name;
@@ -87,8 +105,10 @@ public enum Content {
     private final String strength;
     private final String preference;
     private final List<String> types;
+    private final String frontImgUrl;
+    private final String backImgUrl;
 
-    Content(String code, String name, String comment, String description, String ability, List<String> values, String strength, String preference, List<String> types) {
+    Content(String code, String name, String comment, String description, String ability, List<String> values, String strength, String preference, List<String> types, String frontImgUrl, String backImgUrl) {
         this.code = code;
         this.name = name;
         this.comment = comment;
@@ -98,6 +118,8 @@ public enum Content {
         this.strength = strength;
         this.preference = preference;
         this.types = types;
+        this.frontImgUrl = frontImgUrl;
+        this.backImgUrl = backImgUrl;
     }
 
     public String getCode() {
@@ -134,5 +156,13 @@ public enum Content {
 
     public List<String> getTypes() {
         return types;
+    }
+
+    public String getFrontImgUrl() {
+        return frontImgUrl;
+    }
+
+    public String getBackImgUrl() {
+        return backImgUrl;
     }
 }
