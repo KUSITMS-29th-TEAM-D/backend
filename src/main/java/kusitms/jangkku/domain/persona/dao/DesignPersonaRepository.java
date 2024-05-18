3e0f0149a2348df0803a566009b2bf1f7f7cb541
bbbang105 <2018111366@dgu.ac.kr>
2024-05-19 03:52:50 +0900

    #40 [feat] : 유저로 조회하는 기능 추가

--- src/main/java/kusitms/jangkku/domain/persona/dao/DesignPersonaRepository.java
+++ src/main/java/kusitms/jangkku/domain/persona/dao/DesignPersonaRepository.java
@@ -1,7 +1,9 @@
 package kusitms.jangkku.domain.persona.dao;
 
 import kusitms.jangkku.domain.persona.domain.DesignPersona;
+import kusitms.jangkku.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DesignPersonaRepository extends JpaRepository<DesignPersona,Long> {
+    DesignPersona findByUser(User user);
 }
