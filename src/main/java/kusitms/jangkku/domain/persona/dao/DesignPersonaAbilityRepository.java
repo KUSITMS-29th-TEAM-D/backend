fffc7fc2d87d3ce74fdf71b2cc43576105734b73
bbbang105 <2018111366@dgu.ac.kr>
2024-05-19 03:52:39 +0900

    #40 [feat] : 설계하기 페르소나로 조회하는 기능 추가

--- src/main/java/kusitms/jangkku/domain/persona/dao/DesignPersonaAbilityRepository.java
+++ src/main/java/kusitms/jangkku/domain/persona/dao/DesignPersonaAbilityRepository.java
@@ -1,7 +1,11 @@
 package kusitms.jangkku.domain.persona.dao;
 
+import kusitms.jangkku.domain.persona.domain.DesignPersona;
 import kusitms.jangkku.domain.persona.domain.DesignPersonaAbility;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DesignPersonaAbilityRepository extends JpaRepository<DesignPersonaAbility,Long> {
+    List<DesignPersonaAbility> findAllByDesignPersona(DesignPersona designPersona);
 }
--- src/main/java/kusitms/jangkku/domain/persona/dao/DesignPersonaPlatformRepository.java
+++ src/main/java/kusitms/jangkku/domain/persona/dao/DesignPersonaPlatformRepository.java
@@ -1,7 +1,11 @@
 package kusitms.jangkku.domain.persona.dao;
 
+import kusitms.jangkku.domain.persona.domain.DesignPersona;
 import kusitms.jangkku.domain.persona.domain.DesignPersonaPlatform;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DesignPersonaPlatformRepository extends JpaRepository<DesignPersonaPlatform,Long> {
+    List<DesignPersonaPlatform> findAllByDesignPersona(DesignPersona designPersona);
 }
