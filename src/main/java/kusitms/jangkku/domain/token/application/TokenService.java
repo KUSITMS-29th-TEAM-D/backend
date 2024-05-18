9bb596316dae33eac6968aade8420d4be50594c2
bbbang105 <2018111366@dgu.ac.kr>
2024-05-19 02:46:47 +0900

    #40 [style] : import문 수정

--- src/main/java/kusitms/jangkku/domain/token/application/TokenService.java
+++ src/main/java/kusitms/jangkku/domain/token/application/TokenService.java
@@ -2,8 +2,8 @@ package kusitms.jangkku.domain.token.application;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import kusitms.jangkku.domain.token.dto.response.TokenResponse;
+import kusitms.jangkku.domain.token.dto.TokenDto;
 
 public interface TokenService {
-    TokenResponse reissueAccessToken(HttpServletRequest request, HttpServletResponse response);
+    TokenDto.TokenResponse reissueAccessToken(HttpServletRequest request, HttpServletResponse response);
 }
