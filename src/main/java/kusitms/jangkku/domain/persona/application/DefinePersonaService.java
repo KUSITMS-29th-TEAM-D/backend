16ef44138fbb2c3736625f84acbcd2a8d143515c
bbbang105 <2018111366@dgu.ac.kr>
2024-05-20 04:47:38 +0900

    #18 [feat] : 비로그인 전용 정의하기 페르소나 생성 API 구현 (POST)

--- src/main/java/kusitms/jangkku/domain/persona/application/DefinePersonaService.java
+++ src/main/java/kusitms/jangkku/domain/persona/application/DefinePersonaService.java
@@ -4,6 +4,7 @@ import kusitms.jangkku.domain.persona.dto.DefinePersonaDto;
 
 public interface DefinePersonaService {
     DefinePersonaDto.DefinePersonaResponse createDefinePersona(String authorizationHeader, DefinePersonaDto.DefinePersonaRequest definePersonaRequest);
+    DefinePersonaDto.DefinePersonaResponse createDefinePersonaForSharing(DefinePersonaDto.DefinePersonaRequest definePersonaRequest);
     DefinePersonaDto.DefinePersonaResponse getDefinePersona(String authorizationHeader);
     DefinePersonaDto.DefinePersonaResponse getDefinePersonaForSharing(String definePersonaId);
 }
