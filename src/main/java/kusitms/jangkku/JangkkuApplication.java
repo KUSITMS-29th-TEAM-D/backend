7738a42c29e81a4f05e77d96c04820632858ff3f
bbbang105 <2018111366@dgu.ac.kr>
2024-05-20 00:03:10 +0900

    #43 [refactor] : JpaConfig 로 분리

--- src/main/java/kusitms/jangkku/JangkkuApplication.java
+++ src/main/java/kusitms/jangkku/JangkkuApplication.java
@@ -2,14 +2,10 @@ package kusitms.jangkku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class JangkkuApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JangkkuApplication.class, args);
     }
-
 }
