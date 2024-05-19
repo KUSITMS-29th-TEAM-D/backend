7106269f55a2f2663d56c6058e3a5f609a1479c3
bbbang105 <2018111366@dgu.ac.kr>
2024-05-20 00:07:41 +0900

    #43 [feat] : 돌아보기 페르소나 엔터티 & 레퍼지토리 추가

---
+++ src/main/java/kusitms/jangkku/domain/persona/domain/DiscoverPersona.java
@@ -0,0 +1,36 @@
+package kusitms.jangkku.domain.persona.domain;
+
+import jakarta.persistence.*;
+import kusitms.jangkku.domain.user.domain.User;
+import kusitms.jangkku.global.common.dao.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "discover_personas")
+public class DiscoverPersona extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "users_id", foreignKey = @ForeignKey(name = "discover_personas_fk_users_id"))
+    private User user;
+
+    @Column(name = "category", nullable = false, length = 20)
+    private String category;
+
+    @Column(name = "is_complete", nullable = false)
+    private Boolean isComplete;
+
+    @Builder
+    public DiscoverPersona(User user, String category) {
+        this.user = user;
+        this.category = category;
+        this.isComplete = false;
+    }
+
+    public void updateComplete() {
+        this.isComplete = true;
+    }
+}
