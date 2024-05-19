3c0e242ccf34600fbeca6c1b21fa5262c6ea570f
bbbang105 <2018111366@dgu.ac.kr>
2024-05-20 01:36:06 +0900

    #43 [feat] : Clova에서 공감 & 요약 받는 기능 구현

--- src/main/java/kusitms/jangkku/domain/clova/application/ClovaService.java
+++ src/main/java/kusitms/jangkku/domain/clova/application/ClovaService.java
@@ -3,6 +3,7 @@ package kusitms.jangkku.domain.clova.application;
 import kusitms.jangkku.domain.clova.dto.ClovaDto;
 
 public interface ClovaService {
-    String requestWebClient(ClovaDto.ChatBotRequestDto request);
     String createDesignPersona(String message);
+    String createDiscoverPersonaReaction(String message);
+    String createDiscoverPersonaSummary(String message);
 }
