c6ff1180e073024a6b3b8493443c97a91f7ceae7
emilywin825 <emilywin825@naver.com>
2024-05-21 20:08:08 +0900

    [refactor]ProgramImageKeyWord 엔터티 Getter 추가

--- src/main/java/kusitms/jangkku/domain/program/domain/ProgramsImageKeyword.java
+++ src/main/java/kusitms/jangkku/domain/program/domain/ProgramsImageKeyword.java
@@ -3,9 +3,11 @@ package kusitms.jangkku.domain.program.domain;
 import jakarta.persistence.*;
 import kusitms.jangkku.domain.keyword.domain.Keyword;
 import kusitms.jangkku.global.common.dao.BaseEntity;
+import lombok.Getter;
 
 @Entity
 @Table(name = "programs_image_keywords")
+@Getter
 public class ProgramsImageKeyword extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "keywords_id", foreignKey = @ForeignKey(name = "programs_images_keywords_fk_keywords_id"))
