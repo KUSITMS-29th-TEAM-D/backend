8c6f808c006f014789b29fdc0648d1d6f2ee776c
bbbang105 <2018111366@dgu.ac.kr>
2024-05-22 21:39:59 +0900

    #43 [feat] : 페르소나의 키워드를 가져오는 메서드 추가

--- src/main/java/kusitms/jangkku/domain/persona/dao/DiscoverPersonaKeywordRepository.java
+++ src/main/java/kusitms/jangkku/domain/persona/dao/DiscoverPersonaKeywordRepository.java
@@ -1,7 +1,11 @@
 package kusitms.jangkku.domain.persona.dao;
 
+import kusitms.jangkku.domain.persona.domain.DiscoverPersona;
 import kusitms.jangkku.domain.persona.domain.DiscoverPersonaKeyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DiscoverPersonaKeywordRepository extends JpaRepository<DiscoverPersonaKeyword,Long> {
+    List<DiscoverPersonaKeyword> findAllByDiscoverPersona(DiscoverPersona discoverPersona);
 }
