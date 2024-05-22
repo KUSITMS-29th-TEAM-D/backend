07d1a8532cdc68f0f2bc0ee2269191b4395457b1
emilywin825 <emilywin825@naver.com>
2024-05-23 03:19:45 +0900

    [feat] ProgramParticipantsRepository 추가

--- src/main/java/kusitms/jangkku/domain/user/dto/UserDto.java
+++ src/main/java/kusitms/jangkku/domain/user/dto/UserDto.java
@@ -3,6 +3,8 @@ package kusitms.jangkku.domain.user.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import kusitms.jangkku.domain.program.domain.Branding;
+import kusitms.jangkku.domain.program.domain.SelfUnderstanding;
 import kusitms.jangkku.domain.user.domain.User;
 import kusitms.jangkku.domain.user.domain.UserOnboardingInfo;
 import lombok.AllArgsConstructor;
@@ -81,6 +83,35 @@ public class UserDto {
                     .job(userOnboardingInfo.getJob())
                     .understandingScore(userOnboardingInfo.getUnderstandingScore())
                     .profileImgUrl(userOnboardingInfo.getProfileImgUrl())
+                    .build();
+        }
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class userHomeResponse {
+        private String imageUrl;
+        private String programsTitle;
+        private Long programsId;
+        private String type;
+
+        public static UserDto.userHomeResponse of(Branding branding){
+            return userHomeResponse.builder()
+                    .imageUrl(branding.getBrandingUrl())
+                    .programsTitle(branding.getName())
+                    .programsId(branding.getId())
+                    .type("branding")
+                    .build();
+        }
+
+        public static UserDto.userHomeResponse of(SelfUnderstanding selfUnderstanding){
+            return userHomeResponse.builder()
+                    .imageUrl(selfUnderstanding.getSelfUnderstandingUrl())
+                    .programsTitle(selfUnderstanding.getName())
+                    .programsId(selfUnderstanding.getId())
+                    .type("self-understanding")
                     .build();
         }
     }
