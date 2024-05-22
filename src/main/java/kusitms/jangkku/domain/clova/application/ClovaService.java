7a9f376b32a9d762562e41148087cd251d7329b8
bbbang105 <2018111366@dgu.ac.kr>
2024-05-22 22:49:36 +0900

    #43 [feat] : 돌아보기 페르소나 키워드를 생성하는 메서드 구현

--- src/main/java/kusitms/jangkku/domain/clova/application/ClovaService.java
+++ src/main/java/kusitms/jangkku/domain/clova/application/ClovaService.java
@@ -1,9 +1,8 @@
 package kusitms.jangkku.domain.clova.application;
 
-import kusitms.jangkku.domain.clova.dto.ClovaDto;
-
 public interface ClovaService {
     String createDesignPersona(String message);
     String createDiscoverPersonaReaction(String message);
     String createDiscoverPersonaSummary(String message);
+    String createDiscoverPersonaKeywords(String message);
 }
